9b1c2ad48c1adf56c19ee96795ef3136c1ff4d19
Elena Palmer <c.elena.palmer@gmail.com>
2016-10-31 21:22:06 -0500

    weather script update

--- weather_script.py
+++ weather_script.py
@@ -1,8 +1,12 @@
 import requests
 import datetime
+from token import get_token
+
 end_date = datetime.date.today().strftime("%Y-%m-%d")
-url = 'http://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&locationid=FIPS:27&datatypeid=TMAX&startdate=2016-05-01&enddate=2016-05-01'
+url = 'http://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&datatypeid=TAVG&locationid=FIPS:27&startdate=2016-01-01&enddate=2016-01-31&units=standard'
 url_2 = 'http://www.ncdc.noaa.gov/cdo-web/api/v2/locations?locationcategoryid=CITY&sortfield=name&sortorder=desc&limit=52'
-headers = {'token': 'oZAkdjVeaJXhsSDLcchbcqWUyXlJDdOX'}
+url_3 = 'http://www.ncdc.noaa.gov/cdo-web/api/v2/datatypes/TMAX'
+token = get_token()
+headers = {'token': token}
 response = requests.get(url, headers=headers)
 print (response.text)
